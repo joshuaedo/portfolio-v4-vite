6bf57d9a0fa36bed4262b68a4581eafe82a6c0da
Joshua Edo <joshua.edo01@gmail.com>
2023-06-30 00:49:41 +0000

    Update Slug.jsx

--- src/pages/projects/Slug.jsx
+++ src/pages/projects/Slug.jsx
@@ -1,5 +1,6 @@
 import { projects } from "../../lib/ProjectData.jsx";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 //  title,
 //     src,
@@ -15,27 +16,73 @@ import { useParams } from "react-router-dom";
 //     documentation,
 
 export function Slug() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
   const { slug } = useParams();
   const project = projects.filter((p) => p.slug === slug);
   console.log(project[0]);
 
   return (
     <main className={`flex items-center justify-center`}>
-      <div className="relative min-h-[300vh] w-[90%]">
-        <section className="fixed top-0 pb-[3vh] pt-[15vh] text-center md:flex md:pt-[37vh] md:text-start">
-          <h1 className="section-text uppercase">{project[0].title}</h1>
+      <div className="w-[90%]">
+        <section className="text-center md:flex md:text-start">
+          <h1 className="section-text uppercase">{project[0]?.title}</h1>
         </section>
-        <section className="absolute top-[43vh] z-[5] min-h-[200vh] bg-black">
+
+        <section className="bg-black">
           <div className="flex items-center justify-center">
             <img
               width={1366}
               height={768}
-              alt={project[0].title}
-              src={`/images/projects/${project[0].src}`}
+              alt={project[0]?.title}
+              src={`/images/projects/${project[0]?.src}`}
             />
           </div>
-          <div className=""></div>
-          <div className=""></div>
+
+          <div className="">
+            <p className="">{project[0].overview}</p>
+            <p className="">{project[0]?.description}</p>
+            {project[0]?.documentation && (
+              <p className="">
+                Learn more{" "}
+                <a href={project[0]?.documentation}>
+                  <span className="link">here</span>
+                </a>
+              </p>
+            )}
+            <div>
+              <div>
+                {project[0].href && (
+                  <a href={project[0]?.href}>
+                    <span className="link">LIVE</span>
+                  </a>
+                )}
+              </div>
+              <div>
+                {project[0].code && (
+                  <a href={project[0]?.code}>
+                    <span className="link">CODE</span>
+                  </a>
+                )}
+              </div>
+              <div>
+                {project[0].demo && (
+                  <a href={project[0]?.demo}>
+                    <span className="link">DEMO</span>
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="">{project[0].tech}</div>
+            <div className="">{project[0].year}</div>
+          </div>
+
           <div className=""></div>
         </section>
       </div>

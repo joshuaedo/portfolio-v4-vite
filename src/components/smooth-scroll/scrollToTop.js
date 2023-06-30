d87b49585acb0b8817f2f780f4409276d56cb631
Joshua <joshua.edo01@gmail.com>
2023-06-30 02:28:24 +0100

    another try

--- src/components/smooth-scroll/scrollToTop.js
+++ src/components/smooth-scroll/scrollToTop.js
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default function ScrollToTop() {
-  const { pathname } = useLocation();
-
+function ScrollToTop({ history }) {
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+    return () => {
+      unlisten();
+    }
+  }, []);
 
-  return null;
+  return (null);
 }
+
+export default withRouter(ScrollToTop);
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <App />
   </React.StrictMode>,
 )

9a748ba0fefc1c8b6097fe2d1fab64c26183400e
Joshua Edo <joshua.edo01@gmail.com>
2023-07-01 12:42:37 +0000

    Update Likes.jsx

--- src/pages/about/sections/likes/Likes.jsx
+++ src/pages/about/sections/likes/Likes.jsx
@@ -16,7 +16,7 @@ export default function Likes() {
         <div
           onMouseOver={handleMouseOver}
           onMouseLeave={handleMouseLeave}
-          className="relative h-[45vh] w-[70vw] rounded-lg bg-blue-500 text-black shadow-2xl shadow-green-500/20 md:w-[20vw]"
+          className="relative h-[40vh] w-[70vw] rounded-lg bg-blue-500 text-black shadow-2xl shadow-green-500/20 md:w-[30vw]"
         >
           {isHovered ? (
             <video
@@ -28,19 +28,19 @@ export default function Likes() {
               <source src="/videos/likes.mp4" type="video/mp4" />
             </video>
           ) : (
-            <div className="flex h-full w-full items-center justify-center">
-              <ul className="title-text flex flex-col items-center text-xs">
-                <li>pizza</li>
-                <li>dan brown</li>
-                <li>chess</li>
-                <li>how i met your mother</li>
-                <li>chelsea</li>
-              </ul>
-            </div>
+            <p className="title-text p-5 text-xs">hover?</p>
           )}
         </div>
         <div>
-          <p className="header-text text-7xl text-white">← STUFF I LIKE</p>
+          <p className="header-text text-7xl text-white">STUFF I LIKE</p>
+          <ul className="flex flex-col items-center text-xs">
+            <li className="mb-3 text-lg">↓</li>
+            <li>pizza</li>
+            <li>dan brown</li>
+            <li>chess</li>
+            <li>how i met your mother</li>
+            <li>chelsea</li>
+          </ul>
         </div>
       </div>
     </div>

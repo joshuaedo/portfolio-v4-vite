a9b62c402676cb9476e1cfd901a6fb9bfb381994
Joshua Edo <joshua.edo01@gmail.com>
2023-07-02 22:17:36 +0000

    Update NameMarquee.jsx

--- src/components/name-marquee/NameMarquee.jsx
+++ src/components/name-marquee/NameMarquee.jsx
@@ -2,7 +2,6 @@
 import styles from "./NameMarquee.module.css";
 import { useRef, useEffect } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 
 export default function NameMarquee() {
   const firstText = useRef(null);
@@ -12,17 +11,6 @@ export default function NameMarquee() {
   let direction = -1;
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: (e) => (direction = e.direction * -1),
-      },
-      x: "-500px",
-    });
     requestAnimationFrame(animate);
   }, []);
 
@@ -35,7 +23,7 @@ export default function NameMarquee() {
     gsap.set(firstText.current, { xPercent: xPercent });
     gsap.set(secondText.current, { xPercent: xPercent });
     requestAnimationFrame(animate);
-    xPercent += 0.2 * direction;
+    xPercent += 0.1 * direction;
   };
 
   return (

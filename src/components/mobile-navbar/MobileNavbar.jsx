e439018875d58eaa264e7a18e9c027a19333ee6d
Joshua Edo <joshua.edo01@gmail.com>
2023-07-05 11:33:15 +0000

    Update MobileNavbar.jsx

--- src/components/mobile-navbar/MobileNavbar.jsx
+++ src/components/mobile-navbar/MobileNavbar.jsx
@@ -11,15 +11,15 @@ export default function MobileNavbar() {
   };
 
   return (
-    <div className="static flex h-[12vh] w-full items-center justify-between bg-black bg-opacity-50 p-4 text-lg md:hidden">
+    <div className="static flex h-[12vh] items-center justify-between bg-black bg-opacity-50 p-4 text-lg md:hidden">
       <div className="inline cursor-pointer font-medium">
         <Link to="/">Joshua Edo</Link>
       </div>
-      <div className="relative inline pr-5">
+      <div className="relative">
         <div
-          className={`absolute left-1/2 top-1/2 ${
+          className={`absolute right-2 top-1/2 ${
             show || isActive ? "hidden" : "inline"
-          } z-10 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer pr-5 font-medium`}
+          } z-10 -translate-y-1/2 transform cursor-pointer font-medium`}
           onClick={handleClick}
         >
           MENU+

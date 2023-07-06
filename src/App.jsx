2fee1bd349f01ab71c10bf91a2efdd6cf15a2ec3
Joshua Edo <joshua.edo01@gmail.com>
2023-07-06 15:41:40 +0000

    Update 4 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { HomeLoader } from "./components/loader/Loading.jsx";
-// import LenisScroller from "./components/smooth-scroll/LenisScroller.jsx";
-import LocomotiveScroll from "./components/smooth-scroll/LocomotiveScroll.jsx";
-import Cursor from "./components/cursor/Cursor.jsx";
-import { Router } from "./components/router/Router.jsx";
-import Navbar from "./components/navbar/Navbar.jsx";
-import MobileNavbar from "./components/mobile-navbar/MobileNavbar.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import { ScrollToTop } from "./components/smooth-scroll/ScrollToTop.jsx";
 import Header from "./components/header";
+import { BrowserRouter } from "react-router-dom";
+import Cursor from "./components/cursor/Cursor.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import { Router } from "./components/router/Router.jsx";
+import { HomeLoader } from "./components/loader/Loading.jsx";
 import { HeaderProvider } from "./contexts/HeaderContext.jsx";
+import MobileNavbar from "./components/mobile-navbar/MobileNavbar.jsx";
+import { ScrollToTop } from "./components/smooth-scroll/ScrollToTop.jsx";
+// import LenisScroller from "./components/smooth-scroll/LenisScroller.jsx";
 
 export function App() {
   const [loading, setLoading] = useState(true);
@@ -23,27 +22,25 @@ export function App() {
 
   return (
     <BrowserRouter>
-      <div className="bg-black tracking-wider text-white" id="scroller">
+      <div className="bg-black tracking-wider text-white">
         {loading ? (
           <>
             <HomeLoader />
           </>
         ) : (
           <>
-            {/* <LenisScroller> */}
             <HeaderProvider>
-              <LocomotiveScroll>
-                <ScrollToTop>
-                  <Header />
-                  <Navbar />
-                  <MobileNavbar />
-                  <Cursor />
-                  <Router />
-                  <Footer />
-                </ScrollToTop>
-              </LocomotiveScroll>
+              {/* <LenisScroller> */}
+              <ScrollToTop>
+                <Header />
+                <Navbar />
+                <MobileNavbar />
+                <Cursor />
+                <Router />
+                <Footer />
+              </ScrollToTop>
+              {/* </LenisScroller> */}
             </HeaderProvider>
-            {/* </LenisScroller> */}
           </>
         )}
       </div>

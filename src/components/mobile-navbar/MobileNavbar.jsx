d71c1313c6f8d7b0d3ac172b3aed1b6e4c75c811
Joshua Edo <joshua.edo01@gmail.com>
2023-07-07 08:43:09 +0000

    Update MobileNavbar.jsx

--- src/components/mobile-navbar/MobileNavbar.jsx
+++ src/components/mobile-navbar/MobileNavbar.jsx
@@ -1,7 +1,5 @@
-import gsap from "gsap";
-import { useEffect } from "react";
-import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 import { HeaderContext } from "../../contexts/HeaderContext.jsx";
 
 export default function MobileNavbar() {
@@ -12,17 +10,8 @@ export default function MobileNavbar() {
     setIsActive(!isActive);
   };
 
-  useEffect(() => {
-    gsap.to(".text-reveal", {
-      y: 0,
-      stagger: 0.1,
-      delay: 0.5,
-      duration: 0.2,
-    });
-  }, []);
-
   return (
-    <div className="text-reveal text-reveal__negative static flex h-[12vh] items-center justify-between bg-black bg-opacity-50 p-4 text-lg md:hidden">
+    <div className="text-reveal__fade-in static flex h-[12vh] items-center justify-between bg-black bg-opacity-50 p-4 text-lg md:hidden">
       <div className="inline cursor-pointer font-medium">
         <Link to="/">Joshua Edo</Link>
       </div>

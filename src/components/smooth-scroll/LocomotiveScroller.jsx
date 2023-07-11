0dca27a03aeec6b7d99b5297ac8e89e730c04053
Joshua Edo <joshua.edo01@gmail.com>
2023-07-11 21:22:11 +0000

    testing loco-scroll

--- src/components/smooth-scroll/LocomotiveScroller.jsx
+++ src/components/smooth-scroll/LocomotiveScroller.jsx
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import LocomotiveScroll from "locomotive-scroll";
 
 export default function LocomotiveScroller(props) {
   useEffect(() => {
-    const locomotiveScroll = new LocomotiveScroll({});
-
-    function locomotiveHeightBug() {
-      locomotiveScroll.start();
-    }
-    setInterval(locomotiveHeightBug, 3500);
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+      function locomotiveHeightBug() {
+        locomotiveScroll.start();
+      }
+      setInterval(locomotiveHeightBug, 3500);
+    })();
   }, []);
-
   return <>{props.children}</>;
 }

f77b2f999205b11aea0a07f6c58599a99f98588e
Joshua Edo <joshua.edo01@gmail.com>
2024-01-07 19:03:33 +0000

    refactor: Refactor code and formatting
    Refactored code and formatting to follow consistent style guidelines.

--- src/app/pages/home.jsx
+++ src/app/pages/home.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
-import gsap from 'gsap';
-import { useEffect } from 'react';
-import Marquee from 'react-fast-marquee';
-import styles from '@/styles/name-marquee.module.css';
-import { useState } from 'react';
-import { HomeMeta } from '@/components/layout/meta.jsx';
-import Image, { Arrow } from '@/components/ui/image';
-import { HeroSpotifyPlaylist } from '@/components/ui/spotify';
+import { Link } from "react-router-dom";
+import gsap from "gsap";
+import { useEffect } from "react";
+import Marquee from "react-fast-marquee";
+import styles from "@/styles/name-marquee.module.css";
+import { useState } from "react";
+import { HomeMeta } from "@/components/layout/meta.jsx";
+import Image, { Arrow } from "@/components/ui/image";
+import { HeroSpotifyPlaylist } from "@/components/ui/spotify";
 
 export function Home() {
   useEffect(() => {
-    gsap.to('.text-reveal', {
+    gsap.to(".text-reveal", {
       y: 0,
       stagger: 0.1,
       delay: 0.5,
@@ -20,53 +20,53 @@ export function Home() {
   return (
     <>
       <HomeMeta />
-      <div data-nosnippet className='container-padding home'>
-        <div className='section-padding pb-12 md:pb-0'>
-          <section className='relative'>
-            <div className='text-reveal__container header-text mt-16 '>
-              <div className='flex justify-center lg:justify-start'>
-                <p className='text-reveal'>SOFTWARE</p>
+      <div data-nosnippet className="container-padding home">
+        <div className="section-padding pb-12 md:pb-0">
+          <section className="relative">
+            <div className="text-reveal__container header-text mt-16 ">
+              <div className="flex justify-center lg:justify-start">
+                <p className="text-reveal">SOFTWARE</p>
               </div>
-              <div className='flex justify-center lg:justify-end'>
-                <p className='text-reveal'>DEVELOPER</p>
+              <div className="flex justify-center lg:justify-end">
+                <p className="text-reveal">DEVELOPER</p>
               </div>
             </div>
-            <div className='text-reveal__container mt-3 flex justify-end'>
-              <span className='text-reveal text-reveal__negative title-text line-height max-w-[70%] text-end md:max-w-[30%]'>
+            <div className="text-reveal__container mt-3 flex justify-end">
+              <span className="text-reveal text-reveal__negative title-text line-height max-w-[70%] text-end md:max-w-[30%]">
                 SPECIALIZED IN FRONTEND DEVELOPMENT
               </span>
             </div>
-            <div className='mt-16 hidden justify-end md:flex'>
-              <Link to='mailto:joshua.edo01@gmail.com'>
-                <div className='title-text link flex'>
+            <div className="mt-16 hidden justify-end md:flex">
+              <Link to="mailto:joshua.edo01@gmail.com">
+                <div className="title-text link flex">
                   <span>AVAILABLE FOR HIRE</span>
-                  <Arrow className='ml-1 mt-[2px] h-4 w-4' />
+                  <Arrow className="ml-1 mt-[2px] h-4 w-4" />
                 </div>
               </Link>
             </div>
-            <div className='flex w-full items-center justify-center'>
-              <div className='hero-spotify mt-16 flex h-[14vh] items-center justify-center md:hidden'>
+            <div className="flex w-full items-center justify-center">
+              <div className="hero-spotify mt-16 flex h-[14vh] items-center justify-center md:hidden">
                 <HeroSpotifyPlaylist />
               </div>
             </div>
           </section>
         </div>
-        <div className=''>
-          <section className='relative'>
+        <div className="">
+          <section className="relative">
             <Marquee autoFill>
               <main className={styles.main}>
                 <div className={styles.sliderContainer}>
                   <div className={`${styles.slider} text-reveal__fade-in `}>
                     <p>
-                      <b>Edo</b> • Joshua • <i>THOMAS</i> •{' '}
-                      <span className='font-medium'>Akpan</span> •{' '}
-                      <span className='font-semibold italic'>ANIEKAN</span> • =)
+                      <b>Edo</b> • Joshua • <i>THOMAS</i> •{" "}
+                      <span className="font-medium">Akpan</span> •{" "}
+                      <span className="font-semibold italic">ANIEKAN</span> • =)
                       •
                     </p>
                     <p>
-                      <b>Edo</b> • Joshua • <i>THOMAS</i> •{' '}
-                      <span className='font-medium'>Akpan</span> •{' '}
-                      <span className='font-semibold italic'>ANIEKAN</span> • =)
+                      <b>Edo</b> • Joshua • <i>THOMAS</i> •{" "}
+                      <span className="font-medium">Akpan</span> •{" "}
+                      <span className="font-semibold italic">ANIEKAN</span> • =)
                       •
                     </p>
                   </div>
@@ -75,34 +75,34 @@ export function Home() {
             </Marquee>
           </section>
         </div>
-        <div className='section-padding'>
-          <section className='relative'>
-            <div className='mt-24 text-center md:text-start'>
-              <p className='section-text'>PROJECTS</p>
-              <p className='title-text inline'>A SELECTION OF MY WORK</p>
+        <div className="section-padding">
+          <section className="relative">
+            <div className="mt-24 text-center md:text-start">
+              <p className="section-text">PROJECTS</p>
+              <p className="title-text inline">A SELECTION OF MY WORK</p>
             </div>
-            <div className='mb-16 mt-20 md:mt-40'>
-              <div className='inline items-center justify-around space-y-32 md:flex md:space-y-1'>
-                <div data-scroll data-scroll-speed='0.5' className='space-y-32'>
-                  <ProjectsCard i={4} name='z' slug='z' />
-                  <ProjectsCard i={3} name='prime' slug='prime' />
-                  <ProjectsCard i={5} name='joshuaGPT' slug='joshua-gpt' />
+            <div className="mb-16 mt-20 md:mt-40">
+              <div className="inline items-center justify-around space-y-32 md:flex md:space-y-1">
+                <div data-scroll data-scroll-speed="0.5" className="space-y-32">
+                  <ProjectsCard i={4} name="z" slug="z" />
+                  <ProjectsCard i={3} name="prime" slug="prime" />
+                  <ProjectsCard i={5} name="jquerius" slug="jquerius" />
                 </div>
-                <div data-scroll data-scroll-speed='0.8' className='space-y-32'>
-                  <ProjectsCard i={2} name='sandra' slug='sandra' />
-                  <ProjectsCard i={2} name='google' slug='google' />
+                <div data-scroll data-scroll-speed="0.8" className="space-y-32">
+                  <ProjectsCard i={2} name="sandra" slug="sandra" />
+                  <ProjectsCard i={2} name="google" slug="google" />
                 </div>
               </div>
             </div>
-            <div className='line-height title-text link absolute bottom-10 right-10 hidden max-w-[7rem] text-right text-sm md:right-3 md:flex md:max-w-[10rem]'>
-              <Link to='/projects' className='flex'>
+            <div className="line-height title-text link absolute bottom-10 right-10 hidden max-w-[7rem] text-right text-sm md:right-3 md:flex md:max-w-[10rem]">
+              <Link to="/projects" className="flex">
                 <span>VIEW THE REST OF MY WORK</span>
               </Link>
             </div>
-            <div className='title-text link flex items-center justify-center pt-10 text-xs md:hidden'>
-              <Link to='/projects' className='flex'>
+            <div className="title-text link flex items-center justify-center pt-10 text-xs md:hidden">
+              <Link to="/projects" className="flex">
                 <span>VIEW THE REST OF MY WORK</span>
-                <Arrow className='ml-1 h-4 w-4' />
+                <Arrow className="ml-1 h-4 w-4" />
               </Link>
             </div>
           </section>
@@ -120,25 +120,25 @@ function ProjectsCard({ i, name, slug }) {
   };
 
   return (
-    <div className='relative px-[1.5rem] md:pl-[2rem]'>
+    <div className="relative px-[1.5rem] md:pl-[2rem]">
       <Link to={`/projects/${slug}`}>
         <div
-          className='flex cursor-pointer items-center justify-center'
+          className="flex cursor-pointer items-center justify-center"
           onMouseEnter={handleHover}
           onMouseLeave={handleHover}
         >
-          <div className='relative'>
+          <div className="relative">
             <Image
               webpSrc={`/images/webp/home/${name}/${i}.webp`}
               orgSrc={`/images/original/home/${name}/${i}.jpg`}
               height={1000}
               width={1000}
               alt={name}
-              className='h-[320px] w-[233px] rounded-sm object-cover md:h-[457px] md:w-[332px]'
+              className="h-[320px] w-[233px] rounded-sm object-cover md:h-[457px] md:w-[332px]"
             />
             <div
               className={`absolute left-0 top-0 h-full w-full transition-opacity duration-500 ${
-                isHovered ? 'opacity-50' : 'opacity-0'
+                isHovered ? "opacity-50" : "opacity-0"
               }`}
             >
               <Image
@@ -147,13 +147,13 @@ function ProjectsCard({ i, name, slug }) {
                 height={1000}
                 width={1000}
                 alt={name}
-                className='h-[320px] w-[233px]  rounded-sm object-cover md:h-[457px] md:w-[332px]'
+                className="h-[320px] w-[233px]  rounded-sm object-cover md:h-[457px] md:w-[332px]"
               />
             </div>
           </div>
         </div>
-        <div className='flex items-center justify-center'>
-          <p className='inline w-[233px] pt-3 text-2xl font-thin md:w-[332px] md:text-3xl'>
+        <div className="flex items-center justify-center">
+          <p className="inline w-[233px] pt-3 text-2xl font-thin md:w-[332px] md:text-3xl">
             {name}
           </p>
         </div>
--- src/components/layout/meta.jsx
+++ src/components/layout/meta.jsx
@@ -1,5 +1,5 @@
 function Meta({ title, description, path }) {
-  const siteUrl = 'https://www.joshuaedo.com';
+  const siteUrl = "https://www.joshuaedo.com";
   const metaTitle = `Joshua Edo • ${title}`;
   const metaDescription = description;
   const metaUrl = siteUrl + path;
@@ -7,22 +7,22 @@ function Meta({ title, description, path }) {
   return (
     <header>
       {/* Browser */}
-      <meta name='description' content={metaDescription} />
-      <meta name='current_path' content={path} />
-      <meta name='subject' content={title} />
-      <meta name='url' content={metaUrl} />
-      <meta name='identifier-URL' content={metaUrl} />
-      <meta name='pagename' content={metaTitle} />
-      <meta name='title' content={metaTitle} />
+      <meta name="description" content={metaDescription} />
+      <meta name="current_path" content={path} />
+      <meta name="subject" content={title} />
+      <meta name="url" content={metaUrl} />
+      <meta name="identifier-URL" content={metaUrl} />
+      <meta name="pagename" content={metaTitle} />
+      <meta name="title" content={metaTitle} />
 
       {/* OpenGraph */}
-      <meta property='og:title' content={metaTitle} />
-      <meta property='og:description' content={metaDescription} />
-      <meta property='og:url' content={metaUrl} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:url" content={metaUrl} />
 
       {/* Twitter */}
-      <meta name='twitter:title' content={metaTitle} />
-      <meta name='twitter:description' content={metaDescription} />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDescription} />
 
       <title>{metaTitle}</title>
     </header>
@@ -32,9 +32,9 @@ function Meta({ title, description, path }) {
 export function HomeMeta() {
   return (
     <Meta
-      title='Home'
-      description='Joshua Edo is an experienced Software Developer specialized in building web apps with great functionality, user-friendly interfaces, and strong SEO.'
-      path='/'
+      title="Home"
+      description="Joshua Edo is an experienced Software Developer specialized in building web apps with great functionality, user-friendly interfaces, and strong SEO."
+      path="/"
     />
   );
 }
@@ -42,9 +42,9 @@ export function HomeMeta() {
 export function AboutMeta() {
   return (
     <Meta
-      title='About'
+      title="About"
       description="Craving a mouthwatering pepperoni pizza too? A fan of Chelsea or HIMYM? Nice to meet you! I'm Josh, a Software Developer."
-      path='/about'
+      path="/about"
     />
   );
 }
@@ -52,9 +52,9 @@ export function AboutMeta() {
 export function ProjectsMeta() {
   return (
     <Meta
-      title='Projects'
-      description="View Joshua Edo's recent projects: Spotify, Netflix, Google, joshuaGPT, Apple and much more."
-      path='/projects'
+      title="Projects"
+      description="View Joshua Edo's recent projects: Z, Sandra, Galleria, Spotify, Netflix, Google, jQuerius, Apple and much more."
+      path="/projects"
     />
   );
 }
